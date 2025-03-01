8fa07cee0752a899ba5853e39254e4fe35292841
Davys Lima <davysjunior08@hotmail.com>
2025-02-28 23:47:14 -0300

    feat(dashboard): wrap children in AbilityProvider for role-based access control

--- src/app/dashboard/providers/dashboard-provider.tsx
+++ src/app/dashboard/providers/dashboard-provider.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 "use client";
 
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
+import { AbilityProvider } from "../context/ability-context";
+import { defineAbilityFor } from "~/lib/casl";
 
 type DashboardProviderProps = {
   children: React.ReactNode;
@@ -14,7 +17,9 @@ export const DashboardProvider: React.FC<DashboardProviderProps> = ({
 }) => {
   return (
     <SessionProvider session={session} refetchOnWindowFocus={false}>
-      {children}
+      <AbilityProvider ability={defineAbilityFor(session?.user)}>
+        {children}
+      </AbilityProvider>
     </SessionProvider>
   );
 };
