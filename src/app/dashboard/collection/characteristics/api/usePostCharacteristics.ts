8cd85671695ddbf7d7cd67442e6ef4066c45e4eb
Davys Lima <davys.lima-extern@renault.com>
2025-03-22 12:21:01 -0300

    feat(characteristics): enhance usePostCharacteristics to support multipart/form-data and invalidate cache on success

--- src/app/dashboard/collection/characteristics/api/usePostCharacteristics.ts
+++ src/app/dashboard/collection/characteristics/api/usePostCharacteristics.ts
@@ -1,17 +1,35 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "~/server/api";
+import { GET_CHARACTERISTICS_QUERY_KEY } from "./useGetCharacteristics";
 
-type PostCharacteristics = {
+type PostCharacteristicsApiResponse = {
+  id: number;
   name: string;
-  typeId: number;
   description: string;
+  type: string;
 };
 
-function postCharacteristics(data: PostCharacteristics) {
-  return api.post("/dashboard/characteristics", data);
+function postCharacteristics(data: FormData) {
+  return api.post<PostCharacteristicsApiResponse>(
+    "/dashboard/characteristics",
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    },
+  );
 }
 export function usePostCharacteristics() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postCharacteristics,
+    async onSuccess() {
+      await queryClient.invalidateQueries({
+        queryKey: [GET_CHARACTERISTICS_QUERY_KEY],
+        exact: false,
+      });
+    },
   });
 }
