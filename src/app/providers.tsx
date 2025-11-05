f8391af91db0716090be02949ffa43b7ec0a46bc
Davys Lima <davysjunior08@hotmail.com>
2025-11-05 00:52:17 -0300

    fix: comment out service worker initialization in Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,11 +4,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { type PropsWithChildren } from "react";
 import { Toaster } from "~/components/ui/sonner";
-import { useInitServiceWorker } from "~/hooks/use-init-service-worker";
+// import { useInitServiceWorker } from "~/hooks/use-init-service-worker";
 import { queryClient } from "~/lib/react-query";
 
 export default function Providers({ children }: Readonly<PropsWithChildren>) {
-  useInitServiceWorker();
+  // useInitServiceWorker();
 
   return (
     <QueryClientProvider client={queryClient}>
